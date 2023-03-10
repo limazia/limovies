c5f6536208944611d69f8ddd4da161ff2e117276
Acacio de Lima <32038004+limazia@users.noreply.github.com>
2023-03-09 23:13:56 -0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_KEY = 'a8347441de3ac774a2cbef4710f46109';
-const TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODM0NzQ0MWRlM2FjNzc0YTJjYmVmNDcxMGY0NjEwOSIsInN1YiI6IjYwNWZmYmY3ZWIxNGZhMDAyOGQxNDVkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M2Y8yMBJ-qoWGOsuUNAuLIS54hiJBMG8lhOPDDagsNQ';
+//const API_KEY = '';
+const TOKEN = '';
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
@@ -14,4 +14,3 @@ const api = axios.create({
 });
 
 export const getFilms = () => api.get('movie/popular?language=pt-BR&page=1');
-export const getFilmById = (movie_id: number) => api.get(`movie/${movie_id}?&language=pt-BR`);
